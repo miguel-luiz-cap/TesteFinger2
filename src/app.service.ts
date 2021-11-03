e2f7d97b3df0a7afbd8b8d8cbfae39928757728f
miguel-luiz-cap <miguelluizcap@gmail.com>
2021-11-03 17:47:45 -0300

    teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,7 @@ export class AppService {
     //return this.httpService.get<{ id: number, name: string, email: string, address:any }[]>('https://jsonplaceholder.typicode.com/users');
   }
 
-  async jsonShow(){
+  async jsonShow() {
     const a = await axios.get('https://jsonplaceholder.typicode.com/users');
     
     return a.data;
